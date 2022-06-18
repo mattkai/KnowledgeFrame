3d32d03b24f6d5225b91404f5704621291367978
kaikai123 <827221147@qq.com>
2022-06-18 18:44:46 +0800

    kf-03:update gradle setting (apk build.gradle)

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # KnowledgeFrame
 # 该文章主要是用于学习现在安卓的最新的框架(MVC、MVP、MVVM、MVI等)、
 功能(livedata、ViewModel、RxJava、ConroutinScope等)、自定义View(控件)的开发和分享
+app      app壳可进行第三方注册
+buildSrc 统一管理版本库
+
+day01 搭建组件化项目、统一管理版本库依赖、自动化打包、分包处理
---
+++ buildSrc/src/main/java/LibMoudle.kt
@@ -0,0 +1,43 @@
+/**
+ * 依赖的插件
+ */
+object PluginLibs {
+    const val application = "com.android.application"
+    const val library = "com.android.library"
+    const val kotlinAndroid = "kotlin-android"
+    const val kotlinAndroidExtensions = "kotlin-android-extensions"
+    const val kotlinKapt = "kotlin-kapt"
+}
+
+object AndroidXLibs {
+    const val constraintlayout =  "androidx.constraintlayout:constraintlayout:2.0.4"
+    const val core_ktx = "androidx.core:core-ktx:1.7.0"
+    const val appcompat = "androidx.appcompat:appcompat:1.3.0"
+}
+
+object GoogleLibs {
+    const val material = "com.google.android.material:material:1.4.0"
+}
+
+object TestLibs {
+    const val junit = "junit:junit:4.13.2"
+    const val ext_junit = "androidx.test.ext:junit:1.1.3"
+    const val espresso = "androidx.test.espresso:espresso-core:3.4.0"
+}
+
+object NetLibs {
+    const val retrofit = "com.squareup.retrofit2:retrofit:2.8.1"
+    const val retrofit_converter = "com.squareup.retrofit2:converter-gson:2.8.1"
+    const val retrofit_adapter = "com.squareup.retrofit2:adapter-rxjava2:2.8.1"
+}
+
+object ToolLibs {
+    const val autoSize = "me.jessyan:autosize:1.1.2"
+    const val utilcodex = "com.blankj:utilcodex:1.30.6"
+    const val liveData = "androidx.lifecycle:lifecycle-livedata-ktx:2.3.1"
+    const val viewModel = "androidx.lifecycle:lifecycle-viewmodel-ktx:2.3.1"
+    const val rxjava = "io.reactivex.rxjava2:rxjava:2.2.3"
+    const val rxandroid = "io.reactivex.rxjava2:rxandroid:2.1.0"
+}
+
+
